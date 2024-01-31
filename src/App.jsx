b68fdaaaf0a0f0fcf798fea58722fb82362c4ad6
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2024-01-31 11:45:28 -0300

    all app develop

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Login from "./pages/Login";
+// import HomePage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+
+import City from "./components/City";
+import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import { Suspense, lazy } from "react";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+
+// dist/assets/index-964e06d9.css   31.32 kB │ gzip:   5.23 kB
+// dist/assets/index-6da97668.js   527.32 kB │ gzip: 149.49 kB
+
+function App() {
+  return (
+    <div>
+      <AuthProvider>
+        <CitiesProvider>
+          <BrowserRouter>
+            <Suspense fallback={<SpinnerFullPage />}>
+              <Routes>
+                <Route index element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="login" element={<Login />} />
+                <Route
+                  path="app"
+                  element={
+                    <ProtectedRoute>
+                      <AppLayout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Navigate replace to="cities" />} />
+                  <Route path="cities" element={<CityList />} />
+                  <Route path="cities/:id" element={<City />} />
+                  <Route path="countries" element={<CountryList />} />
+                  <Route path="form" element={<Form />} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+              </Routes>
+            </Suspense>
+          </BrowserRouter>
+        </CitiesProvider>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
